38d0b16a748650233fb0547b7bc9232cca79e09d
DESKTOP-HCT2M34\quang <quangtuyen1993@gmail.com>
2021-04-11 00:22:12 +0700

    fix scheduler

--- src/pages/scheduler_maintain/index.jsx
+++ src/pages/scheduler_maintain/index.jsx
@@ -192,7 +192,6 @@ export default function SchedulerMaintain() {
               "createByUser",
               "startTime",
               "endTime",
-              "status",
               "content",
             ]}
           />
